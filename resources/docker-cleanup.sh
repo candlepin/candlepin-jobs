69f49c1c9a67db42e97ddfd3482744638d494c49
James Molet <jmolet@redhat.com>
2017-08-10 15:20:01 -0400

    more agressive docker cleanup

--- resources/docker-cleanup.sh
+++ resources/docker-cleanup.sh
@@ -5,6 +5,9 @@ function jssh(){
 }
 
 for HOST in $(echo $DOCKER_HOSTS | sed "s/,/ /g"); do
-	jssh ${HOST} "docker images | grep '<none>' | awk '{print \$3}' | xargs -r docker rmi"
-    jssh ${HOST} "docker ps -aq | xargs -r docker rm -f"
+	# jssh ${HOST} "docker images | grep '<none>' | awk '{print \$3}' | xargs -r docker rmi"
+  # more agressive
+  jssh ${HOST} "docker images -q | xargs -r docker rmi -f"
+  jssh ${HOST} "docker ps -aq | xargs -r docker rm -f"
+  jssh ${HOST} "docker volume ls -q | xargs -r docker volume rm"
 done
