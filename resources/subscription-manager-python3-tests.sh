087beb3f6b914c985d7fd7193d4fc6ceca1c55c1
Kevin Howell <khowell@redhat.com>
2018-01-18 09:54:48 -0500

    Fallback to virtualenv-3
    If for whatever reason python-virtualenv is not installed, but
    python3-virtualenv is, the script will still work.

--- resources/subscription-manager-python3-tests.sh
+++ resources/subscription-manager-python3-tests.sh
@@ -18,7 +18,7 @@ echo "sha" "${sha1}"
 
 
 sudo yum-builddep subscription-manager.spec  # ensure we install any missing rpm deps
-virtualenv env -p python3 --system-site-packages || true
+virtualenv env -p python3 --system-site-packages || virtualenv-3 env --system-site-packages || true
 source env/bin/activate
 pip install -I -r test-requirements.txt
 
