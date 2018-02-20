e7b0d9fd1ef4244f6c3f7fbf57e2debef225f8ac
Jiri Hnidek <jhnidek@redhat.com>
2018-02-20 09:43:25 +0100

    * Renamed new job to CandlepinUpdatePerformanceBaseline * Improved description of new job * Removed triggering of job using cron

--- jobs/candlepinUpdatePerformanceBaseline.groovy
+++ jobs/candlepinUpdatePerformanceBaseline.groovy
@@ -2,9 +2,9 @@ import jobLib.rhsmLib
 
 String baseFolder = rhsmLib.candlepinJobFolder
 
-job("$baseFolder/CandlepinPerformanceMaintanance") {
+job("$baseFolder/CandlepinUpdatePerformanceBaseline") {
     disabled()
-    description('This job runs candlepin maintenance of performance tests')
+    description('This job updates the performance test baseline data.')
     label('rhsm')
     wrappers {
         preBuildCleanup()
@@ -24,11 +24,8 @@ job("$baseFolder/CandlepinPerformanceMaintanance") {
     logRotator{
         numToKeep(10)
     }
-    triggers {
-        cron('10 3 * * 7')
-    }
     steps {
-        ansiblePlaybook('ansible/regular_maintenance.yml') {
+        ansiblePlaybook('ansible/baseline_maintenance.yml') {
             inventoryPath('/etc/performanceInventory')
             credentialsId('fe2c79db-3166-4e61-8996-a8e7de7fbb5c')
             additionalParameters('--extra-vars=\"caracalla_branch=${caracalla_branch}\"')
