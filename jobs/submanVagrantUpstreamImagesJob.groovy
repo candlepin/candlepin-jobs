15b4ce40c92e23262e67a25ae432f97167e21390
Kevin Howell <khowell@redhat.com>
2018-11-06 14:35:28 -0500

    Fix syntax issues in vagrant images job

--- jobs/submanVagrantUpstreamImagesJob.groovy
+++ jobs/submanVagrantUpstreamImagesJob.groovy
@@ -1,16 +1,16 @@
 import jobLib.rhsmLib
 String baseFolder = rhsmLib.submanJobFolder
 
-pipeline_helper = job("$baseFolder/vagrant-upstream-images") {
+job("$baseFolder/vagrant-upstream-images") {
     description('builds centos, fedora, etc. vagrant images for subman development')
     label('rhsm-packer')
     wrappers {
         colorizeOutput()
-    }
-    credentialsBinding {
-        string('VAGRANT_CLOUD_TOKEN', 'VAGRANT_CLOUD_TOKEN')
+        credentialsBinding {
+            string('VAGRANT_CLOUD_TOKEN', 'VAGRANT_CLOUD_TOKEN')
+        }
     }
     steps {
-	shell(readFileFromWorkspace('resources/subman-vagrant-images.sh'))
+        shell(readFileFromWorkspace('resources/subman-vagrant-images.sh'))
     }
 }
