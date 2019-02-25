dafb452fc9f926972c2b4f57fad4a349c0797d84
James Molet <jmolet@redhat.com>
2019-02-25 12:24:37 -0500

    updating DockerCleanup job to use slave labels

--- resources/docker-cleanup.sh
+++ resources/docker-cleanup.sh
@@ -1,17 +1,26 @@
 #!/bin/bash -x
 
-function jssh(){
-  ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no jenkins@${1} "${2}"
-}
+#docker images -q | xargs -r docker rmi -f
+#docker ps -aq | xargs -r docker rm -f
+#docker network rm $(docker network ls | awk '$3 == "bridge" && $2 != "bridge" { print $1 }')
+#docker volume ls -q | xargs -r docker volume rm
 
-for HOST in $(echo $DOCKER_HOSTS | sed "s/,/ /g"); do
-	# jssh ${HOST} "docker images | grep '<none>' | awk '{print \$3}' | xargs -r docker rmi"
-  # more agressive
-  jssh ${HOST} "docker images -q | xargs -r docker rmi -f"
-  jssh ${HOST} "docker ps -aq | xargs -r docker rm -f"
-  jssh ${HOST} "docker volume ls -q | xargs -r docker volume rm"
-  jssh ${HOST} 'docker network rm $(docker network ls | awk '"'"'$3 == "bridge" && $2 != "bridge" { print $1 }'"'"')'
-  if [ "$RESTART_DOCKER" = 'true' ]; then
-    jssh ${HOST} "sudo systemctl restart docker"
-  fi
-done
+# this does all of the above now
+if [ "$CLEAR_BUILD_CACHE" = 'true' ]; then
+  docker system prune -a -f
+  # add --volumes to the above whenever on Docker 17.06.1
+  docker volume prune -f
+else
+  # everything but the build cache
+  docker image prune -a -f
+  docker volume prune -f
+  docker container prune -f
+  docker network prune -f
+fi
+
+if [ "$RESTART_DOCKER" = 'true' ]; then
+  sudo systemctl restart docker
+fi
+
+
+
