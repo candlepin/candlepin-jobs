61abd1a36a07c0e6b0338162534ba1a72f1d4be9
Jiri Hnidek <jhnidek@redhat.com>
2019-11-18 13:58:43 +0100

    Do not wait for confirmation (dnf builddep sub-man.spec)

--- src/resources/subscription-manager-stylish-tests.sh
+++ src/resources/subscription-manager-stylish-tests.sh
@@ -18,7 +18,7 @@ echo "sha1:" "${sha1}"
 cd $WORKSPACE
 
 sudo dnf clean expire-cache
-sudo dnf builddep subscription-manager.spec  # ensure we install any missing rpm deps
+sudo dnf builddep -y subscription-manager.spec  # ensure we install any missing rpm deps
 virtualenv env -p python2
 source env/bin/activate
 
