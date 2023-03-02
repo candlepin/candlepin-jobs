67ae94e84d9f6feab7acef1259e977fd428658ef
William Poteat <wpoteat@redhat.com>
2023-03-02 13:15:31 -0500

    CANDLEPIN-513: Update to use podman in candlepin

--- src/resources/candlepin-podman-build.sh
+++ src/resources/candlepin-podman-build.sh
@@ -9,8 +9,5 @@ export CONTAINER_ENGINE="podman"
 "${CONTAINER_ENGINE}" --version
 
 "${CONTAINER_ENGINE}" login -u "$CANDLEPIN_QUAY_BOT_USER" -p "$CANDLEPIN_QUAY_BOT_TOKEN" quay.io
-./docker/build-images -p -c
+./containers/build-images -p -c
 
-sudo setenforce 0
-cd docker/gating-test-images
-./build.sh
