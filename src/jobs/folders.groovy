bf2a95777afc736bb23cdec4452b53c5d15004a2
Pino Toscano <ptoscano@redhat.com>
2023-03-31 12:31:26 +0200

    Drop all the subscription-manager bits
    subscription-manager now uses GitHub Actions for CI, so all the
    downstream jobs are no more needed.

--- src/jobs/folders.groovy
+++ src/jobs/folders.groovy
@@ -1,9 +1,5 @@
 import jobLib.rhsmLib
 
-folder(rhsmLib.submanJobFolder) {
-    description("This is folder for the subscription manager pipeline jobs.")
-}
-
 folder(rhsmLib.candlepinJobFolder) {
     description("This is folder for the candlepin pipeline jobs.")
 }
--- src/resources/subman-vagrant-images.sh
+++
@@ -1,2 +0,0 @@
-ansible-galaxy install -r requirements.yml
-packer build -force subman-centos7.json
