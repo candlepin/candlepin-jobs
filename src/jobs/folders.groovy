84a4abae6666609f92c8b1c555899f92d5739f28
Pino Toscano <ptoscano@redhat.com>
2023-08-16 16:04:27 +0200

    Drop all the virt-who bits
    virt-who now uses GitHub Actions for CI, so all the downstream jobs
    are no more needed.

--- src/jobs/folders.groovy
+++ src/jobs/folders.groovy
@@ -3,7 +3,3 @@ import jobLib.rhsmLib
 folder(rhsmLib.candlepinJobFolder) {
     description("This is folder for the candlepin pipeline jobs.")
 }
-
-folder(rhsmLib.virtwhoJobFolder) {
-    description("This is folder for the virt who pipeline jobs.")
-}
